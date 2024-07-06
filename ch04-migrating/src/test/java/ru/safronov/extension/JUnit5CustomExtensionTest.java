539ff147cc449f83b6eaeb6318a699e66851c586
Vladimir Safronov <safron5000@mail.ru>
2024-07-06 13:22:38 +0300

    add differences between JUnit4 rules and JUnit5 extension models

---
+++ ch04-migrating/src/test/java/ru/safronov/extension/JUnit5CustomExtensionTest.java
@@ -0,0 +1,13 @@
+package ru.safronov.extension;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(CustomExtension.class)
+public class JUnit5CustomExtensionTest {
+
+  @Test
+  void myCustomRuleTest() {
+    System.out.println("Call of a test method");
+  }
+}
