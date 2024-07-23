658269368b64387a592c0396995f44cd4dd1596f
Vladimir Safronov <safron5000@mail.ru>
2024-07-23 20:07:31 +0300

    add practise with maven ch10

---
+++ ch10-maven/src/main/java/ru/safronov/Passenger.java
@@ -0,0 +1,25 @@
+package ru.safronov;
+
+public class Passenger {
+
+  private final String identifier;
+  private final String name;
+
+  public Passenger(String identifier, String name) {
+    this.identifier = identifier;
+    this.name = name;
+  }
+
+  public String getIdentifier() {
+    return identifier;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public String toString() {
+    return "Passenger " + getName() + " with identifier: " + getIdentifier();
+  }
+}
